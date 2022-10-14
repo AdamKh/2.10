1125a562a890fc8df25406a430e53ef5d5db7d64
AdamKh <adamkhashiev2004@mail.ru>
2022-10-14 20:23:57 +0300

    added progs

---
+++ ind.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+
+def sum_mod_min(*args):
+    if args:
+        mni = 0
+        mn = abs(args[0])
+        s = 0
+        i = 0
+        for ch in args:
+            if abs(ch) < mn:
+                mni = i
+                mn = ch
+            i += 1
+        for ch in args[mni + 1:]:
+            s += abs(ch)
+        return s
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print(sum_mod_min(-100, 2, -3, 4))
---
+++ zadaniya.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import math
+
+
+def sr_geom(*args):
+    if args:
+        values = [float(arg) for arg in args]
+        n = len(values)
+        pr = 1
+        k = 0
+        for k in range(n):
+            pr *= values[k]
+            k += 1
+        g = math.pow(pr, 1 / k)
+        return g
+    else:
+        return None
+
+
+def sr_garm(*args):
+    if args:
+        values = [float(arg) for arg in args]
+        n = len(values)
+        s = 0
+        for k in range(n):
+            s += 1/values[k]
+        g = n / s
+        return g
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    print(sr_geom(1, 2, 3, 4))
+    print(sr_garm(1, 2, 3, 4))
